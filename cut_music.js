77999adbc11cda77c59851d6819c655f4feae37a
Máté Balázs <wrckzz@gmail.com>
2018-06-19 11:34:07 +0200

    last element removal

--- cut_music.js
+++ cut_music.js
@@ -6,17 +6,17 @@ fs.readFile('songs', 'utf-8', (error, data) => {
     if (error) {
         throw error;
     }
-    var splitSongs = data.split('\n');
-    queue.init(splitSongs, (item, finished) => {
+    var splitSongs = data.split('\n');    
+    queue.init(splitSongs.splice(0,splitSongs.length-1), (item, finished) => {
         var split = item.split(";"),
             duration = split[1].trim().replace(/\(|\)/gi, ""),
             songName = split[0],
             begin = duration.split("-")[0],
             end = duration.split("-")[1];
         console.log(`ffmpeg -i file.mp3 -ss ${begin} -to ${end} -c copy ${songName}.mp3`);
-        child_process.exec(`ffmpeg -i file.mp3 -ss ${begin} -to ${end} -c copy \"${songName}.mp3\"`, (error, stdout, stderr) => {
+        child_process.exec(`ffmpeg -y -i file.mp3 -ss ${begin} -to ${end} -c copy \"${songName}.mp3\"`, (error, stdout, stderr) => {
             console.log(`${error} ${stdout} ${stderr}`);
-            if (stderr.includes("frame=")) {
+            if (stderr.includes("muxing")) {
                 finished();
             }
         });
