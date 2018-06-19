1aeef61bba27e5cbcf7c3921fac93f6fb3aede14
Máté Balázs <wrckzz@gmail.com>
2018-06-19 09:36:15 +0200

    working

--- cut_music.js
+++ cut_music.js
@@ -1,5 +1,6 @@
 const fs = require('fs'),
-    queue = require('./queue');
+    queue = require('./queue'),
+    child_process = require('child_process');
 
 fs.readFile('songs', 'utf-8', (error, data) => {
     if (error) {
@@ -7,7 +8,18 @@ fs.readFile('songs', 'utf-8', (error, data) => {
     }
     var splitSongs = data.split('\n');
     queue.init(splitSongs, (item, finished) => {
-        var split = item.split(" ");
+        var split = item.split(";"),
+            duration = split[1].trim().replace(/\(|\)/gi, ""),
+            songName = split[0],
+            begin = duration.split("-")[0],
+            end = duration.split("-")[1];
+        console.log(`ffmpeg -i file.mp3 -ss ${begin} -to ${end} -c copy ${songName}.mp3`);
+        child_process.exec(`ffmpeg -i file.mp3 -ss ${begin} -to ${end} -c copy \"${songName}.mp3\"`, (error, stdout, stderr) => {
+            console.log(`${error} ${stdout} ${stderr}`);
+            if (stderr.includes("frame=")) {
+                finished();
+            }
+        });
     }, () => {
         console.log("Done.")
     });
